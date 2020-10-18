1e4abbb02f8e492bf80ca17703abbeb72af235b2
teenyda <teenyda@gmail.com>
2020-10-18 21:56:41 +0800

    修改产品完善

--- src/views/product/info/index.vue
+++ src/views/product/info/index.vue
@@ -4,7 +4,11 @@
       <el-row>
         <el-col :span="8">
           <el-card :body-style="{ padding: '0px' }">
-            <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" height="300px" class="image">
+            <img
+              src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+              height="300px"
+              class="image"
+            >
             <div style="padding: 14px;">
               <span>好吃的汉堡</span>
               <span class="price">
@@ -13,25 +17,48 @@
               </span>
               <div class="bottom clearfix">
                 <time class="time">好吃的汉堡描述商品描述好吃的汉堡描述商品描述...</time>
-                <el-button type="text" class="button">更改</el-button>
+                <el-button
+                  type="text"
+                  class="button"
+                >更改</el-button>
               </div>
             </div>
           </el-card>
         </el-col>
 
-        <el-col :span="15" :offset="1">
+        <el-col
+          :span="15"
+          :offset="1"
+        >
           <el-card :body-style="{ padding: '0px' }">
-            <el-carousel trigger="click" height="300px" @change="onChange">
-              <el-carousel-item v-for="(item,index) in bannerList" :key="index" autoplay="false">
-                <h3 v-if="!item.image" class="small">{{ item }}</h3>
-                <el-image v-if="item.image" :src="item.image"></el-image>
+            <el-carousel
+              trigger="click"
+              height="300px"
+              @change="onChange"
+            >
+              <el-carousel-item
+                v-for="(item,index) in bannerList"
+                :key="index"
+                autoplay="false"
+              >
+                <h3
+                  v-if="!item.image"
+                  class="small"
+                >{{ item }}</h3>
+                <el-image
+                  v-if="item.image"
+                  :src="item.image"
+                ></el-image>
               </el-carousel-item>
             </el-carousel>
             <div style="padding: 14px;">
               <span>水果幻灯片</span>
               <div class="bottom clearfix">
                 <time class="time">{{ currentDate }}</time>
-                <el-button type="text" class="button">更改</el-button>
+                <el-button
+                  type="text"
+                  class="button"
+                >更改</el-button>
               </div>
             </div>
           </el-card>
@@ -48,22 +75,45 @@
           </el-card>
         </el-col>
 
-        <el-col :span="15" :offset="1">
+        <el-col
+          :span="15"
+          :offset="1"
+        >
           <el-card shadow="hover">
             商品规格:
-            <el-tooltip class="item" effect="dark" content="specContent" placement="top">
+            <el-tooltip
+              effect="dark"
+              content="specContent"
+              placement="top"
+            >
               <span class="el-tag el-tag--light">
                 小果（斤）
-                <i class="el-icon-edit el-tag__edit" v-on:click="editTag(item, index)"></i>
-                <i class="el-tag__close el-icon-close" v-on:click="removeTag(item, index)"></i>
+                <i
+                  class="el-icon-edit el-tag__edit"
+                  v-on:click="editTag(item, index)"
+                ></i>
+                <i
+                  class="el-tag__close el-icon-close"
+                  v-on:click="removeTag(item, index)"
+                ></i>
               </span>
             </el-tooltip>
 
-            <el-tooltip class="item" effect="dark" content="specContent" placement="top">
+            <el-tooltip
+              effect="dark"
+              content="specContent"
+              placement="top"
+            >
               <span class="el-tag el-tag--light">
                 中果（斤）
-                <i class="el-icon-edit el-tag__edit" v-on:click="editTag(item, index)"></i>
-                <i class="el-tag__close el-icon-close" v-on:click="removeTag(item, index)"></i>
+                <i
+                  class="el-icon-edit el-tag__edit"
+                  v-on:click="editTag(item, index)"
+                ></i>
+                <i
+                  class="el-tag__close el-icon-close"
+                  v-on:click="removeTag(item, index)"
+                ></i>
               </span>
             </el-tooltip>
           </el-card>
@@ -71,49 +121,86 @@
       </el-row>
     </div>
 
-    <div class="item-box" style="min-height: 500px;">
+    <div
+      class="item-box"
+      style="min-height: 500px;"
+    >
 
-       <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
-        <el-tab-pane label="商品介绍" name="first">
-          <div class="demo-image__lazy" >
-            <el-image v-for="url in urls" :key="url" :src="url"></el-image>
+      <el-tabs
+        v-model="activeName"
+        type="card"
+        @tab-click="handleClick"
+      >
+        <el-tab-pane
+          label="商品介绍"
+          name="first"
+        >
+          <div class="demo-image__lazy">
+            <el-image
+              v-for="url in urls"
+              :key="url"
+              :src="url"
+            ></el-image>
           </div>
         </el-tab-pane>
 
-        <el-tab-pane label="图片编辑" name="second">
+        <el-tab-pane
+          label="图片编辑"
+          name="second"
+        >
           <el-card shadow="hover">
             <draggable
-              tag="transition-group"
-              :componentData="componentData"
+              tag="el-collapse"
               :list="list"
+              :component-data="getComponentData()"
               class="list-group"
               draggable=".item"
-              :animation="100"
+              ghost-class="ghost"
+              :animation="200"
               @start="dragging = true"
-              @end="dragging = false"
-            >
-                <div v-for="url in urls" :key="url" class="item">
-                    <el-image
-                      style="width: 100px; height: 100px"
-                      :src="url"
-                      fit="fill">
-                    </el-image>
+              @end="dragging = false">
+              <div
+                class="list-group-item item"
+                v-for="(url, index) in list"
+                :key="url.id"
+              >
+                <el-image
+                  style="width: 100px; height: 100px"
+                  :src="url.img"
+                  fit="fill"
+                >
+                </el-image>
+
+                <span class="img-order">排序序号：{{ index }}</span>
+
+                <div style="flex: 1;text-align: right;">
+                  <el-button type="warning" size="mini" round>修改图片</el-button>
+                  <el-button type="danger" size="mini" round>删除图片</el-button>
                 </div>
-                <div
-              slot="footer"
-              class="btn-group list-group-item"
-              role="group"
-              aria-label="Basic example"
-              key="footer"
-            >
-          <button class="btn btn-secondary" @click="add">Add</button>
-                </div>
+              </div>
+              <div
+                slot="footer"
+                class="btn-group list-group-item"
+                role="group"
+                aria-label="Basic example"
+                key="footer"
+                style="text-align: right;margin: 30px 0px;"
+              >
+                <el-button type="success" size="mini" round>添加图片</el-button>
+                <el-button type="primary" size="mini" round>保存序号</el-button>
+              </div>
             </draggable>
           </el-card>
         </el-tab-pane>
 
-        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+        <el-tab-pane
+          label="角色管理"
+          name="third"
+        >角色管理</el-tab-pane>
+        <el-tab-pane
+          label="定时任务补偿"
+          name="fourth"
+        >定时任务补偿</el-tab-pane>
       </el-tabs>
 
     </div>
@@ -121,7 +208,7 @@
 </template>
 
 <script>
-  import draggable from 'vuedraggable'
+import draggable from "vuedraggable";
 
 export default {
   name: "footerslot",
@@ -131,92 +218,145 @@ export default {
     return {
       currentDate: new Date(),
       bannerCount: 5,
-      bannerList: ['无幻灯片，请添加', '无幻灯片，请添加', '无幻灯片，请添加'],
+      bannerList: ["无幻灯片，请添加", "无幻灯片，请添加", "无幻灯片，请添加"],
       list: [
-        { name: "John", text: "", id: 0 , img: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'},
-        { name: "Joao", text: "", id: 1 , img: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'},
-        { name: "Jean", text: "", id: 2 , img: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'}
+        {
+          name: "John",
+          text: "",
+          id: 0,
+          img:
+            "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+        },
+        {
+          name: "Joao",
+          text: "",
+          id: 1,
+          img:
+            "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
+        },
+        {
+          name: "Jean",
+          text: "",
+          id: 2,
+          img:
+            "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
+        },
+        {
+          name: "Jean",
+          text: "",
+          id: 3,
+          img:
+            "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+        },
+        {
+          name: "Jean",
+          text: "",
+          id: 4,
+          img:
+            "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
+        },
+        {
+          name: "Jean",
+          text: "",
+          id: 5,
+          img:
+            "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
+        },
       ],
       dragging: false,
-      activeName: 'first',
+      activeName: "first",
       urls: [
-        'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-        'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
+        "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
+        "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
         // 'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-        'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-        'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-        'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-        'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg'
+        "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
+        "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
+        "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
+        "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
       ],
       componentData: {
         props: {
           type: "transition",
-          name: "flip-list"
-        }
-      }
-    }
+          url: "flip-list",
+        },
+      },
+    };
   },
   methods: {
     ajaxGetBanner() {
       return [
         {
           productId: 12345,
-          productName: '新疆葡萄价格美丽',
-          image: 'https://img.zcool.cn/community/013de756fb63036ac7257948747896.jpg'
-        }
-      ]
+          productName: "新疆葡萄价格美丽",
+          image:
+            "https://img.zcool.cn/community/013de756fb63036ac7257948747896.jpg",
+        },
+      ];
     },
 
-    onChange(index) {
-    },
+    onChange(index) {},
 
-    editTag(item, index) {
+    editTag(item, index) {},
 
-    },
-
-    removeTag(item, index) {
-
-    },
+    removeTag(item, index) {},
 
     removeAt(idx) {
       this.urls.splice(idx, 1);
     },
-    add: function() {
+    add: function () {
       // this.urls.push({ name: "Juan " + id, id: id++ })
     },
-    replace: function() {
-      this.urls = ['https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg']
+    replace: function () {
+      this.urls = [
+        "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
+      ];
     },
 
-    addBanner() {
-
-    },
+    addBanner() {},
 
     handleClick(tab, event) {
       console.log(tab, event);
+    },
+    handleChange() {
+      console.log('changed');
+    },
+    inputChanged(value) {
+      this.activeNames = value;
+    },
+    getComponentData() {
+      return {
+        on: {
+          change: this.handleChange,
+          input: this.inputChanged
+        },
+        attrs:{
+          wrap: true
+        },
+        props: {
+          value: this.activeNames
+        }
+      }
     }
-
   },
 
   created() {
-    const banners = this.ajaxGetBanner()
-    const length = this.bannerCount - banners.length
+    const banners = this.ajaxGetBanner();
+    const length = this.bannerCount - banners.length;
     // 幻灯片数量不足用文字代替图片
     // console.log(length)
     for (let i = 0; i < length; i++) {
-      banners.push('无幻灯片，请添加')
+      banners.push("无幻灯片，请添加");
     }
-    this.bannerList = banners
+    this.bannerList = banners;
   },
 
   components: {
-    draggable
+    draggable,
   },
 }
 </script>
 
 <style scoped>
-
   .time {
     font-size: 13px;
     color: #999;
@@ -239,12 +379,12 @@ export default {
 
   .clearfix:before,
   .clearfix:after {
-      display: table;
-      content: "";
+    display: table;
+    content: "";
   }
 
   .clearfix:after {
-      clear: both
+    clear: both;
   }
 
   .el-carousel__item h3 {
@@ -257,11 +397,11 @@ export default {
   }
 
   .el-carousel__item:nth-child(2n) {
-     background-color: #99a9bf;
+    background-color: #99a9bf;
   }
 
-  .el-carousel__item:nth-child(2n+1) {
-     background-color: #d3dce6;
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
   }
 
   .price {
@@ -293,7 +433,7 @@ export default {
   }
 
   .el-tag__edit:hover {
-    color: #FFF;
+    color: #fff;
     background-color: #409eff;
   }
   .el-tag + .el-tag {
@@ -322,21 +462,27 @@ export default {
   }
   .item {
     border: 1px solid #eee;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    color: #6d6d6d;
+    font-size: 13px;
   }
   .item-box {
     padding: 30px;
     margin-top: 15px;
     font-size: 15px;
-    box-shadow: 0 0 5px rgba(202,203,203,0.5);
-    -webkit-box-shadow: 0 0 5px rgba(202,203,203,0.5);
-    -moz-box-shadow: 0 0 5px rgba(202,203,204,0.5);
+    /* box-shadow: 0 0 5px rgba(202, 203, 203, 0.5);
+    -webkit-box-shadow: 0 0 5px rgba(202, 203, 203, 0.5);
+    -moz-box-shadow: 0 0 5px rgba(202, 203, 204, 0.5); */
   }
 
-  .item-box:hover {
-    box-shadow: 0 0 5px #0091FF;
-    -webkit-box-shadow: 0 0 5px #0091FF;
-    -moz-box-shadow: 0 0 5px #0091FF;
-  }
+  /* .item-box:hover {
+    box-shadow: 0 0 5px #0091ff;
+    -webkit-box-shadow: 0 0 5px #0091ff;
+    -moz-box-shadow: 0 0 5px #0091ff;
+  } */
+
   .ghost {
     opacity: 0.5;
     background: #c8ebfb;
@@ -355,5 +501,8 @@ export default {
   }
   .no-move {
     transition: transform 0s;
+  }
+  .img-order {
+    margin-left: 30px;
   }
 </style>
